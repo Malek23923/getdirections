def9dece61658bed0962e4b788c6c2b6cd0670ee
hutch <hutch@midwales.net>
2012-10-17 23:44:59 +0100

    simplified html header and doctype

--- getdirections_box.tpl.php
+++ getdirections_box.tpl.php
@@ -3,38 +3,27 @@
 /**
  * @file getdirections_box.tpl.php
  * Template file for colorbox implementation
+ * Copy this file to your theme's folder and edit to match your theme.
  */
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
-  "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
-  <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"
-  xmlns:content="http://purl.org/rss/1.0/modules/content/"
-  xmlns:dc="http://purl.org/dc/terms/"
-  xmlns:foaf="http://xmlns.com/foaf/0.1/"
-  xmlns:og="http://ogp.me/ns#"
-  xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
-  xmlns:sioc="http://rdfs.org/sioc/ns#"
-  xmlns:sioct="http://rdfs.org/sioc/types#"
-  xmlns:skos="http://www.w3.org/2004/02/skos/core#"
-  xmlns:xsd="http://www.w3.org/2001/XMLSchema#">
-
-<head profile="http://www.w3.org/1999/xhtml/vocab">
+<!DOCTYPE html>
+<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
 <!-- getdirections_box -->
 <head>
   <title><?php print $head_title; ?></title>
   <?php print $head; ?>
   <?php print $styles; ?>
   <?php print $scripts; ?>
-<style>
-  body {
-    width: 800px;
-  }
-  #page {
-    min-width: 800px;
-    width: 800px;
-  }
-</style>
+  <style>
+    body {
+      width: 800px;
+    }
+    #page {
+      min-width: 800px;
+      width: 800px;
+    }
+  </style>
 </head>
 <body class="<?php print $body_classes; ?>">
   <div id="page-wrapper"><div id="page">
