6ebfce4855344d83e599c8136dced0027cf08dd1
hutch <hutch@midwales.net>
2012-12-03 21:32:29 +0000

    improved getdirections_box template file

--- getdirections_box.tpl.php
+++ getdirections_box.tpl.php
@@ -1,37 +1,38 @@
 <?php
 
 /**
- * @file
+ * @file getdirections_box.tpl.php
  * Template file for colorbox implementation
+ * Copy this file to your theme's folder and edit to match your theme.
  */
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
+<!DOCTYPE html>
+<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
 <!-- getdirections_box -->
 <head>
   <title><?php print $head_title; ?></title>
   <?php print $head; ?>
   <?php print $styles; ?>
   <?php print $scripts; ?>
-<style>
-  body {
-    width: 800px;
-  }
-  #page {
-    min-width: 800px;
-    width: 800px;
-  }
-</style>
+  <style>
+    body {
+      width: 800px;
+    }
+    #page {
+      min-width: 800px;
+      width: 800px;
+    }
+  </style>
 </head>
 <body class="<?php print $body_classes; ?>">
-  <div id="page"><div id="page-inner">
-    <div id="main"><div id="main-inner" class="clear-block">
-      <div id="content"><div id="content-inner">
+  <div id="page-wrapper"><div id="page">
+    <div id="main-wrapper" class="clearfix"><div id="main" class="clearfix">
+      <div id="content" class="column"><div class="section">
         <?php if ($title): ?>
           <h2 class="title"><?php print $title; ?></h2>
         <?php endif; ?>
-        <div id="content-area">
+        <div class="content">
           <?php print $content; ?>
         </div>
       </div></div>
